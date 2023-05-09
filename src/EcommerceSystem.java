e7e98c421c70d342b7ea2506247a11e63c9317d7
Yahya Ehab <yahyaehab1856@gmail.com>
2023-05-10 00:30:20 +0300

    .

---
+++ src/EcommerceSystem.java
@@ -0,0 +1,8 @@
+package src;
+
+public class EcommerceSystem {
+    public void registerUser(){};
+    public void registerAdmin(){};
+    public void loginUser(){};
+    public void loginAdmin(){};
+}
---
+++ src/Item.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Item {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+    private int category_id;
+    private int user_id;
+    private int order_id;
+    private int admin_id;
+    private int status;
+}
