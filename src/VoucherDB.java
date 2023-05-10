149bc93736a635f435b081eb910644ba39e37bd5
Yahya Ehab <yahyaehab1856@gmail.com>
2023-05-10 21:59:34 +0300

    connected the StockDB and VoucherDB classes with the database files StockDB.db and VoucherDB.db

---
+++ src/VoucherDB.java
@@ -0,0 +1,39 @@
+package src;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.File;
+
+public class VoucherDB {
+     /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:\\Users\\Yahya Ehab\\IdeaProjects\\E-commerce-System\\VoucherDB.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connect();
+    }
+}
